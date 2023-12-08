a94490d420bd68808fd538d9984912c7f0440cf7
astriel-git <lpatriciopro@outlook.com>
2023-12-08 10:38:19 -0300

    -feat fix sms again

--- src/scripts/sms.ts
+++ src/scripts/sms.ts
@@ -5,19 +5,23 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID || "your_account_sid";
 const authToken = process.env.TWILIO_AUTH_TOKEN || "your_auth_token";
 
 // Create a Twilio client instance
-const client = twilio(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken);
 
 // Replace with your Verify service SID
 const verifyServiceSid = "VA1f28bcbc51c26770786687207b2dc1b9";
 
 // Function to send a verification code via SMS
-export async function sendSMS(recipientPhoneNumber: string, message: string) {
+export async function sendSMS(
+  recipientPhoneNumber: string,
+  customMessage: string
+) {
   try {
     const sentMessage = await client.messages.create({
-      body: message,
-      from: "your_twilio_phone_number", // Replace with your Twilio phone number
+      body: customMessage,
+      from: "+12019030981", // Replace with your Twilio phone number
       to: recipientPhoneNumber,
     });
+
     console.log("Custom SMS SID:", sentMessage.sid);
   } catch (error) {
     console.error("Error sending custom SMS:", error);
